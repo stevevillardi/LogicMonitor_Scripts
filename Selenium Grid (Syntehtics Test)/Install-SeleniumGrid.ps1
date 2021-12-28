a00cfa2dc27b46129130a649b85099837d47aa8e
Steve Villardi <steven.villardi@logicmonitor.com>
2021-12-27 21:32:44 -0500

    GRID instrucitons

---
+++ Selenium Grid (Syntehtics Test)/Install-SeleniumGrid.ps1
@@ -0,0 +1,158 @@
+#Requires -RunAsAdministrator
+[CmdletBinding()] 
+param (
+    [String]$DefaultDirectory = "C:\LogicMonitor-Selenium", #Default file path for all things Selnium
+
+    [String]$ScheduleTaskName = "LogicMonitor-Selenium GRID 4 Startup", #Name used to create scheduled task.
+
+    [Boolean]$DisableChromeAutoUpdates = $true, #Adds registry file to disable chrome upates, allows for manual control over updates as the installed chromedriver must match the installed chrome version for slenium to work.
+
+    [Int]$SeleniumPort = 4444, #Default port for selenium grid hub
+
+    [Int]$SeleniumMaxSessions = 4 #Default maximum number of sessions, will default to the number of cores if set size is larger then available core count.
+    )
+#Force TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+If(!(Test-Path $DefaultDirectory)){
+    Write-Host "[INFO]: Default directory not found, creating directory: $DefaultDirectory"
+    $NewDefaultDirectory = New-Item -ItemType Directory -Name "LogicMonitor-Selenium" -Path "C:\"
+
+    If(!$NewDefaultDirectory){
+        Write-Error "Unable to create default directory: $DefaultDirectory"
+        Return
+    }
+}
+Else{
+    Write-Host "[INFO]: Default directory found, using directory ($DefaultDirectory) for configuration of Selenium GRID"
+}
+
+#Test if Java is currently installed
+$JavaVersionInstalled = Get-ChildItem "HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty pschildname -Last 1
+If(!$JavaVersionInstalled){
+    #Download folder for Java installer
+    $JavaDownloadsDirectory = "$DefaultDirectory\64bit-java.exe"
+    
+    If(Test-Path -Path $DefaultDirectory){
+        #Continue with java installation
+        Try {
+            Write-Host "[INFO]: Java not detected, downloading latest version of Java..."
+            #Lastest URL for 64-bit Windows
+            $JavaURL = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245060_d3c52aa6bfa54d3ca74e617f18309292" 
+            Invoke-WebRequest -Uri $JavaURL -OutFile $JavaDownloadsDirectory
+            Write-Host "[INFO]: Installing latest version of Java..."
+            $JavaProcess = Start-Process -FilePath $JavaDownloadsDirectory -ArgumentList "/s REBOOT=ReallySuppress" -Wait -PassThru
+            $JavaProcess.waitForExit()
+            $JavaVersionInstalled = (Get-Command java -ErrorAction SilentlyContinue).Version -join (".")
+            Remove-Item $JavaDownloadsDirectory -Confirm:$false
+            Write-Host "[INFO]: Latest version of Java has been successfully installed."
+        } 
+        Catch [Exception] {
+            Write-Error $_.Exception.Message
+            Return
+        }
+    }
+    Else{
+        Write-Error "[ERROR]: Unable to download Java installer, please manually install java to continue"
+        Return
+    }
+}
+Else {
+    Write-Host "[INFO]: Java version $JavaVersionInstalled is already installed, skipping install process."
+}
+
+#Test is Chrome is installed and if not download and install
+$ChromeTest = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe' -ErrorAction SilentlyContinue
+If (!$ChromeTest) {
+    Write-Host "[INFO]: Downloading Chrome"
+    $Installer = "$DefaultDirectory\chrome_installer.exe"
+    Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer
+    Write-Host "[INFO]: Running silent install of Chrome"
+    Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait
+    Remove-Item $Installer -Confirm:$false
+    $ChromePath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').Path
+    $ChromeVersion = ((Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo).ProductVersion
+}
+Else {
+    $ChromeVersion = ((Get-Item $ChromeTest.'(Default)').VersionInfo).ProductVersion
+    $ChromePath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').Path
+    Write-Host "[INFO]: Previous version of Google Chrome already detected: $($ChromeVersion). Skipping Chrome install"
+}
+
+#Disable Chrome updates to stop chromedriver version missmatch
+$DisableUpdatesKey = (Get-ItemProperty -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Update" -ErrorAction SilentlyContinue).AutoUpdateCheckPeriodMinutes
+If(!$DisableUpdatesKey -and $DisableChromeAutoUpdates){
+    Write-Host "[INFO]: Setting registry key to disabled Chrome auto updates"
+    [Microsoft.Win32.Registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Update", "AutoUpdateCheckPeriodMinutes", 0)
+}
+
+#Install latest ChromeDriver.
+If (!$(Test-Path -Path "$DefaultDirectory\chromedriver.exe")) {
+    Write-Host "[INFO]: Downloading Chromedriver for Selenium"
+    $ChromeDriverVersion = Invoke-RestMethod -Method Get -Uri "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+    $ChromeDriverZip = "$DefaultDirectory\chromedriver_win32.zip"
+    Invoke-RestMethod -Method get -Uri "https://chromedriver.storage.googleapis.com/$ChromeDriverVersion/chromedriver_win32.zip" -OutFile $ChromeDriverZip
+    Expand-Archive -Path $ChromeDriverZip -Force
+    Remove-Item -Path $ChromeDriverZip -Confirm:$false
+    If ($(Test-Path -Path $DefaultDirectory)) {
+        Write-Host "[INFO]: Moving Chromedriver into chrome install directory"
+        Move-Item -Path "chromedriver_win32\chromedriver.exe" -Destination $DefaultDirectory -Force
+        Remove-Item -Path "chromedriver_win32" -Confirm:$false -Force
+    }
+    Else {
+        Write-Host "[INFO]: Chrome directory not detected, unable to move chromedriver into correct location"
+        Return
+    }
+}
+Else {
+    Write-Host "[INFO]: Skipping Chromedrive install since a version is already been detected at: $DefaultDirectory"
+}
+
+#URL for seleniumGrid, need to figure out way to grab latest version automatically
+$SeleniumGridURL = "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.1.jar"
+$SeleniumJarFile = $SeleniumGridURL.Split("/")[-1]
+If(!(Test-Path "$DefaultDirectory\$SeleniumJarFile")){
+    Write-Host "[INFO]: Selenium grid not detected, downloading version $([System.IO.Path]::GetFileNameWithoutExtension($SeleniumJarFile))"
+    Invoke-WebRequest -Uri $SeleniumGridURL -OutFile "$DefaultDirectory\$SeleniumJarFile"
+}
+Else{
+    Write-Host "[INFO]: Selenium grid already detected, skipping download"
+}
+
+#Create Startup script and scheduled task
+$BatchScriptContent = "cmd /c start /min java -Dwebdriver.chrome.driver=./chromedriver.exe -jar $SeleniumJarFile standalone --port $SeleniumPort --max-sessions $SeleniumMaxSessions"
+$BatchScriptFile = Set-Content "$DefaultDirectory\selenium-startup.bat" $BatchScriptContent -Encoding ASCII
+$ExistingScheduledTask = Get-ScheduledTask -TaskPath \ -TaskName $ScheduleTaskName -ErrorAction SilentlyContinue
+If(!$ExistingScheduledTask){
+    Try{
+        $Trigger = New-ScheduledTaskTrigger -AtStartup
+        $Action = New-ScheduledTaskAction -Execute "$DefaultDirectory\selenium-startup.bat" -WorkingDirectory $DefaultDirectory
+        $Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+        $Task = New-ScheduledTask -Action $Action -Description "Startup Script for Selenium Grid 4" -Trigger $Trigger -Principal $Principal
+        $ScheduledTask = Register-ScheduledTask -InputObject $Task -TaskName $ScheduleTaskName
+        If($ScheduledTask){
+            Write-Host "[INFO]: Successfully registered scheduled task for Selenium startup"
+        }
+        Else{
+            Write-Error "[ERROR]: Unable to create schedule task for automated startup: "
+        }
+    }
+    Catch [Exception] {
+        Write-Error $_.Exception.Message
+        Return
+    }
+    #Run scheduled task so selenium is running
+    $StartScheduledTask = Start-ScheduledTask -TaskName $ScheduleTaskName
+    Write-Host "[INFO]: Successfully started scheduled task"
+    $HostAddress = (Get-NetIPAddress -AddressFamily IPv4 -AddressState Preferred -ErrorAction SilentlyContinue | Where-Object {$_.InterfaceAlias -notlike "*loopback*"}).IPAddress
+    If($HostAddress){
+        Write-Host "[INFO]: Selenium GRID started under LOCAL SYSTEM account, verify status using URL: http://$HostAddress`:4444"
+    }
+    Else{
+        Write-Host "[INFO]: Selenium GRID started under LOCAL SYSTEM account, verify status using URL: http://localhost:4444"
+    }
+
+}
+Else {
+    Write-Host "[INFO]: Existing scheduled task ($ScheduleTaskName) already exists, skipping creation"
+}
---
+++ Selenium Grid (Syntehtics Test)/README.md
@@ -0,0 +1,127 @@
+# Selenium Grid Version 4 Installation and Configuration (Using Chrome)
+
+### Step 1: Ensure you have a windows/linux collector deployed to you LogicMonitor Portal
+
+#### Info: https://www.logicmonitor.com/support/collectors/collector-installation/installing-collectors
+
+### Step 2: Follow the setup instructions for Windows or Linux depending on your collector OS:
+
+#### Step 2a: Windows Selenium GRID Setup (Automatic)
+
+Run the following commands from an administrative PowerShell session to automatically prepare your machine to run Selenium (can be same host as collector):
+
+```powershell
+#Download and run directly from PS session
+Invoke-RestMethod -Uri "TBD" | Invoke-Expression
+```
+
+#### Step 2a: Linux/Container Selenium GRID Setup (Automatic)
+
+Docker-based deployment is very easy to accomplish. There are docker images available for use in a number of configurations on the Docker Website.
+
+You can use the tags on the Docker website to filter by which image you may need. Here are some of the search results for three possible GRID Configurations.
+
+**Standalone Chrome Docker Image:**
+https://hub.docker.com/r/selenium/standalone-chrome/tags?page=1&ordering=last_updated
+
+**Selenium GRID Hub Docker Image:**
+https://hub.docker.com/r/selenium/hub/tags?page=1&ordering=last_updated
+
+**Selenium GRID Node Docker Image:**
+https://hub.docker.com/r/selenium/node-chrome/tags?page=1&ordering=last_updated
+
+#### Step 2b: Manual Selenium Setup Windows
+
+Run through the following steps to manually prepare your host to run Selenium, skip this step if using automatic configuration scripts:
+
+1.  Download and install the latest version of Google Chrome : https://www.google.com/chrome/
+2.  Download the latest stable release of the ChromeDriver for your installed version of Google Chrome: https://chromedriver.chromium.org/downloads
+    - Extract the `chromdriver` executable and place into a dedicated folder on your host (ex. C:\LogicMonitor-Selenium)
+3.  Download the latest release of Selenium Server (GRID): https://www.selenium.dev/downloads/
+    - Copy the downloaded `selenium-server-xxx.jar` file into the dedicated directory
+4.  Download and install the latest version of Java: https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245060_d3c52aa6bfa54d3ca74e617f18309292
+5.  Create a .bat file in the dedicated directory called **selenium-startup.bat** with the following content (make sure to update the areas indicated):
+
+```
+cmd /c start /min java -Dwebdriver.chrome.driver=./chromedriver.exe -jar selenium-server-<VERSION>.jar standalone --port <DESIRED_PORT> --max-sessions <MAX-SESSIONS>
+```
+
+6. Create a Windows scheduled task that triggers on startup and as an action executes the batch script created in the previous step:
+
+```
+Run As: SYSTEM
+Triggers: At Startup
+Action: Start A Program
+Program/Script: C:\LogicMonitor-Selenium\selenium-startup.bat
+Start In: C:\LogicMonitor-Selenium\
+```
+
+#### Step 2b: Manual Selenium Setup Linux/Containers
+
+If you make use of one of the pre-configured Docker images for the Selenium GRID - then no additional configuration for the GRID is needed.
+
+However, if you would like to manually make a Docker container that contains the Selenium GRID, by creating a hub witht nodes, you can do so by using the following example:
+
+1. Create a Docker network:
+
+```
+$ docker network create grid
+```
+
+2. Start a Hub:
+
+```
+$ docker run -d -p 4442-4444:4442-4444 --net grid --name selenium-hub selenium/hub:4.0.0
+```
+
+3. Start a Chrome Node by using environment variables:
+
+```
+$ docker run -d --net grid --name selenium-chrome -e SE_EVENT_BUS_HOST=selenium-hub  -e SE_EVENT_BUS_PUBLISH_PORT=4442 -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 selenium/node-chrome:4.0.0
+```
+
+4. Start an Edge Node:
+
+```
+$ docker run -d --net grid --name selenium-edge -e SE_EVENT_BUS_HOST=selenium-hub  -e SE_EVENT_BUS_PUBLISH_PORT=4442 -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 selenium/node-edge:93.0-edgedriver-93.0-grid-4.0.0
+```
+
+### Step 3: Install the Selenium IDE for Chrome
+
+1. Install and activate the chrome extension from the chrome web store: https://chrome.google.com/webstore/detail/selenium-ide/mooikfkahbdckldjjndioackbalphokd?hl=en
+
+### Step 4: Record new Selenium IDE Project
+
+1. Click on Selenium extension in Chrome
+2. Click on `Record a new test in a new project`
+3. Name your project
+4. Set the base url for your synthetic test
+5. Record your test and click on `stop recording` once completed, give your test a name
+6. Optionally add in some assert statements or other non recorded steps required for testing
+7. Replay your test from the IDE window and verify it operates as expected, make any adjustments/cleanup needed until satisfied
+
+### Step 5: Export Selenium Side File
+
+1. Click the save button and export your side file to your local machine
+
+### Step 6: Add Selenium GRID URL to collector configuration
+
+1. Ensure you collector is on EA 30.104 or higher
+2. Edit the collector wrapper.conf configuraiton and set your GRID url:
+
+```
+Example:
+synthetics.selenium.environment.grid.url=http://172.31.18.161:4444
+```
+
+3. Restart collector service
+4. Repeat for other collectors as needed
+
+### Step 7: Create Synthetics Check Resource
+
+1. Under the resources tab click Add -> Synthetics Website Test
+2. Enter a name and description for the test
+3. Upload your side file from the prevous step
+4. Select which tests you wish to import to LM
+5. Choose the polling interval and select which collectors to use for the test. Only collectors with valid **synthetics.selenium.environment.grid.url** configurations will show in this list
+6. Set any Authentication type and finish the creation wizard
